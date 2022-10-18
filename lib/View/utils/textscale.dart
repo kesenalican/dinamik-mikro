761d368a30c926215d1320ca1db1c5bd4a98c683
Alican Kesen <46851204+kesenalican@users.noreply.github.com>
2022-10-18 09:25:17 +0300

    extra files deleted

--- lib/View/utils/textscale.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-Widget fixTextScale(BuildContext context, Widget? child) {
-  final MediaQueryData data = MediaQuery.of(context);
-  return MediaQuery(
-    data: data.copyWith(textScaleFactor: 1.0),
-    child: child!,
-  );
-}
